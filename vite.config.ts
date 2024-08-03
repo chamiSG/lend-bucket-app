9d08ae7945cab94fcbda4beadebb1a2674961db5
chamisg <snoblefuentes@gmail.com>
2024-08-03 21:08:23 +0400

    add config vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import vercel from 'vite-plugin-vercel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // plugins: [react()],
+  plugins: [react(), vercel()],
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_HOST || 'http://127.0.0.1:8000',
+        target: process.env.VITE_API_HOST || 'http://95.216.84.201:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
